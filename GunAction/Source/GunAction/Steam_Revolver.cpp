f736f99375d1dfb6bef3581d0d9cff50fde5ba93
star <KurosawaReo.Job@gmail.com>
2026-01-21 10:28:54 +0900

    プログラムの整理など。まだクラッシュバグは直ってない

--- GunAction/Source/GunAction/Steam_Revolver.cpp
+++ GunAction/Source/GunAction/Steam_Revolver.cpp
@@ -18,76 +18,40 @@ ASteam_Revolver::ASteam_Revolver()
 	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
 	RootComponent = DefaultSceneRoot;
 
-	// 発砲音声コンポーネント
-	S_Revolver_Shot_03_Cue = CreateDefaultSubobject<UAudioComponent>(TEXT("S_Revolver_Shot_03_Cue"));
-	S_Revolver_Shot_03_Cue->SetupAttachment(DefaultSceneRoot);
-	S_Revolver_Shot_03_Cue->bAutoActivate = false;
-	S_Revolver_Shot_03_Cue->SetVolumeMultiplier(1.0f); 
-
-	S_Revolver_Shot_04_Cue = CreateDefaultSubobject<UAudioComponent>(TEXT("S_Revolver_Shot_04_Cue"));
-	S_Revolver_Shot_04_Cue->SetupAttachment(DefaultSceneRoot);
-	S_Revolver_Shot_04_Cue->bAutoActivate = false;
-	S_Revolver_Shot_04_Cue->SetVolumeMultiplier(1.0f);
-
-	S_Revolver_Shot_02_Cue = CreateDefaultSubobject<UAudioComponent>(TEXT("S_Revolver_Shot_02_Cue"));
-	S_Revolver_Shot_02_Cue->SetupAttachment(DefaultSceneRoot);
-	S_Revolver_Shot_02_Cue->bAutoActivate = false;
-	S_Revolver_Shot_02_Cue->SetVolumeMultiplier(1.0f);
-
-	S_Revolver_Shot_05_Cue = CreateDefaultSubobject<UAudioComponent>(TEXT("S_Revolver_Shot_05_Cue"));
-	S_Revolver_Shot_05_Cue->SetupAttachment(DefaultSceneRoot);
-	S_Revolver_Shot_05_Cue->bAutoActivate = false;
-	S_Revolver_Shot_05_Cue->SetVolumeMultiplier(1.0f);
-
-	// マズルフラッシュパーティクル
-	PS_Muzzleflash_Revolver = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("PS_Muzzleflash_Revolver"));
-	PS_Muzzleflash_Revolver->SetupAttachment(DefaultSceneRoot);
-	PS_Muzzleflash_Revolver->bAutoActivate = false;
-
-	// スケルタルメッシュ（リボルバーの本体）
-	Steam_Revolver = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Steam_Revolver"));
-	Steam_Revolver->SetupAttachment(DefaultSceneRoot);
-
 	// 弾丸とシェルコンポーネント
-	Bullet_4 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet_4"));
-	Bullet_4->SetupAttachment(DefaultSceneRoot);
-
-	Shell_4 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shell_4"));
-	Shell_4->SetupAttachment(Bullet_4);
-
-	Bullet_5 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet_5"));
-	Bullet_5->SetupAttachment(DefaultSceneRoot);
-
-	Shell_5 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shell_5"));
-	Shell_5->SetupAttachment(Bullet_5);
-
 	Bullet_1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet_1"));
-	Bullet_1->SetupAttachment(DefaultSceneRoot);
-
+	Bullet_1->SetupAttachment(Steam_Revolver);
 	Shell_1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shell_1"));
 	Shell_1->SetupAttachment(Bullet_1);
 
 	Bullet_2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet_2"));
-	Bullet_2->SetupAttachment(DefaultSceneRoot);
-
+	Bullet_2->SetupAttachment(Steam_Revolver);
 	Shell_2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shell_2"));
 	Shell_2->SetupAttachment(Bullet_2);
 
 	Bullet_3 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet_3"));
-	Bullet_3->SetupAttachment(DefaultSceneRoot);
-
+	Bullet_3->SetupAttachment(Steam_Revolver);
 	Shell_3 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shell_3"));
 	Shell_3->SetupAttachment(Bullet_3);
+
+	Bullet_4 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet_4"));
+	Bullet_4->SetupAttachment(Steam_Revolver);
+	Shell_4 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shell_4"));
+	Shell_4->SetupAttachment(Bullet_4);
+
+	Bullet_5 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet_5"));
+	Bullet_5->SetupAttachment(Steam_Revolver);
+	Shell_5 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shell_5"));
+	Shell_5->SetupAttachment(Bullet_5);
+
+	Bullet_6 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet_6"));
+	Bullet_6->SetupAttachment(Steam_Revolver);
+	Shell_6 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shell_6"));
+	Shell_6->SetupAttachment(Bullet_6);
 
 	// マズル
 	Muzzle = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Muzzle"));
 	Muzzle->SetupAttachment(Steam_Revolver);
-
-	Bullet_6 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet_6"));
-	Bullet_6->SetupAttachment(Steam_Revolver);
-
-	Shell_6 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Shell_6"));
-	Shell_6->SetupAttachment(Bullet_6);
 
 	// ボックスコリジョン
 	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
@@ -103,11 +67,42 @@ ASteam_Revolver::ASteam_Revolver()
 	S_Revolver_Cylinder_Chamber_Close_Cue->SetupAttachment(DefaultSceneRoot);
 	S_Revolver_Cylinder_Chamber_Close_Cue->bAutoActivate = false;
 	S_Revolver_Cylinder_Chamber_Close_Cue->SetVolumeMultiplier(1.0f);
+
 	// ショット1発砲音声
 	S_Revolver_Shot_01_Cue = CreateDefaultSubobject<UAudioComponent>(TEXT("S_Revolver_Shot_01_Cue"));
 	S_Revolver_Shot_01_Cue->SetupAttachment(Muzzle);
 	S_Revolver_Shot_01_Cue->bAutoActivate = false;
 	S_Revolver_Shot_01_Cue->SetVolumeMultiplier(1.0f);
+
+	// マズルフラッシュパーティクル
+	PS_Muzzleflash_Revolver = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("PS_Muzzleflash_Revolver"));
+	PS_Muzzleflash_Revolver->SetupAttachment(DefaultSceneRoot);
+	PS_Muzzleflash_Revolver->bAutoActivate = false;
+
+	// スケルタルメッシュ（リボルバーの本体）
+	Steam_Revolver = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Steam_Revolver"));
+	Steam_Revolver->SetupAttachment(DefaultSceneRoot);
+
+	// 発砲音声コンポーネント
+	S_Revolver_Shot_03_Cue = CreateDefaultSubobject<UAudioComponent>(TEXT("S_Revolver_Shot_03_Cue"));
+	S_Revolver_Shot_03_Cue->SetupAttachment(DefaultSceneRoot);
+	S_Revolver_Shot_03_Cue->bAutoActivate = false;
+	S_Revolver_Shot_03_Cue->SetVolumeMultiplier(1.0f);
+
+	S_Revolver_Shot_04_Cue = CreateDefaultSubobject<UAudioComponent>(TEXT("S_Revolver_Shot_04_Cue"));
+	S_Revolver_Shot_04_Cue->SetupAttachment(DefaultSceneRoot);
+	S_Revolver_Shot_04_Cue->bAutoActivate = false;
+	S_Revolver_Shot_04_Cue->SetVolumeMultiplier(1.0f);
+
+	S_Revolver_Shot_02_Cue = CreateDefaultSubobject<UAudioComponent>(TEXT("S_Revolver_Shot_02_Cue"));
+	S_Revolver_Shot_02_Cue->SetupAttachment(DefaultSceneRoot);
+	S_Revolver_Shot_02_Cue->bAutoActivate = false;
+	S_Revolver_Shot_02_Cue->SetVolumeMultiplier(1.0f);
+
+	S_Revolver_Shot_05_Cue = CreateDefaultSubobject<UAudioComponent>(TEXT("S_Revolver_Shot_05_Cue"));
+	S_Revolver_Shot_05_Cue->SetupAttachment(DefaultSceneRoot);
+	S_Revolver_Shot_05_Cue->bAutoActivate = false;
+	S_Revolver_Shot_05_Cue->SetVolumeMultiplier(1.0f);
 }
 
 void ASteam_Revolver::BeginPlay()
@@ -169,11 +164,11 @@ void ASteam_Revolver::DisableAllCollisions()
 //test
 void SetMeshActive(TObjectPtr<UStaticMeshComponent> Mesh, bool bActive)
 {
-	Mesh->SetVisibility(bActive);
+	/*Mesh->SetVisibility(bActive);
 	Mesh->SetCollisionEnabled(
 		bActive ? ECollisionEnabled::QueryAndPhysics : ECollisionEnabled::NoCollision
 	);
-	Mesh->SetComponentTickEnabled(bActive);
+	Mesh->SetComponentTickEnabled(bActive);*/
 }
 
 //銃のメッシュを無効化.
